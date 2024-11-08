8652ee71e132f49002bddc766403d76ae186c24e
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2024-11-08 09:06:45 -0300

    Initial commit

---
+++ src/test/java/com/gmail/hamiltonjr2010/EscritoresApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.hamiltonjr2010;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscritoresApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
